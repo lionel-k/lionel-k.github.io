e4bab53aeb7044f3f1813a5a1b1f2fa3ef0b10d0
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-01-15 22:26:25 +0100

    Update styles for LanguagePageClient component

--- app/books/[language]/LanguagePageClient.tsx
+++ app/books/[language]/LanguagePageClient.tsx
@@ -24,7 +24,7 @@ export default function LanguagePageClient() {
 
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
         <BreadcrumbNav
           items={[
             { name: "Home", href: "/" },
