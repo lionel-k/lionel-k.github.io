e0bd32a837be7ac4a68d89e2776b2ed2c224871b
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-01-17 13:18:26 +0100

    Add new languages with 'coming soon' status

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,18 +10,18 @@ export const LANGUAGES = [
     slug: "kirundi",
     coverImage: "/bgs/kirundi-bg.png",
     amazonUrl: "#",
+    comingSoon: false,
     discoverBooks:
       "Discover our collection of Kirundi books, available with English and French translations.",
-    comingSoon: false,
   },
   {
     name: "Kinyarwanda",
     slug: "kinyarwanda",
     coverImage: "/bgs/kinyarwanda-bg.png",
     amazonUrl: "#",
+    comingSoon: false,
     discoverBooks:
       "Discover our collection of Kinyarwanda books, available with English and French translations.",
-    comingSoon: false,
   },
   // Other languages with 'coming soon'
   {
@@ -29,9 +29,161 @@ export const LANGUAGES = [
     slug: "amharic",
     coverImage: "/bgs/amharic-bg.png",
     amazonUrl: "#",
+    comingSoon: true,
     discoverBooks:
       "Discover our collection of Amharic books, available with English or French translations.",
-    comingSoon: true,
   },
-  // Add more languages as needed
+  {
+    name: "Bambara",
+    slug: "bambara",
+    coverImage: "/bgs/bambara-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Introduce children to Bambara with simple and easy-to-understand learning books.",
+  },
+  {
+    name: "Ewe",
+    slug: "ewe",
+    coverImage: "/bgs/ewe-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Teach kids Ewe with beginner-friendly books focused on basic vocabulary.",
+  },
+  {
+    name: "Haitian Creole",
+    slug: "haitian-creole",
+    coverImage: "/bgs/haitian-creole-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Help children learn Haitian Creole with fun and easy-to-follow books.",
+  },
+  {
+    name: "Hausa",
+    slug: "hausa",
+    coverImage: "/bgs/hausa-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Introduce kids to Hausa with simple learning books for early vocabulary building.",
+  },
+  {
+    name: "Igbo",
+    slug: "igbo",
+    coverImage: "/bgs/igbo-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Make learning Igbo easy for kids with beginner-friendly vocabulary books.",
+  },
+  {
+    name: "Kikuyu",
+    slug: "kikuyu",
+    coverImage: "/bgs/kikuyu-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Help children start learning Kikuyu with basic word books designed for kids.",
+  },
+  {
+    name: "Lingala",
+    slug: "lingala",
+    coverImage: "/bgs/lingala-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Teach kids basic Lingala words with easy-to-understand books.",
+  },
+  {
+    name: "Luganda",
+    slug: "luganda",
+    coverImage: "/bgs/luganda-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Introduce kids to Luganda with simple and fun learning books.",
+  },
+  {
+    name: "Malagasy",
+    slug: "malagasy",
+    coverImage: "/bgs/malagasy-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Help children learn basic Malagasy words with beginner-friendly books.",
+  },
+  {
+    name: "Oromo",
+    slug: "oromo",
+    coverImage: "/bgs/oromo-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Teach kids Oromo with simple books focused on essential vocabulary.",
+  },
+  {
+    name: "Shona",
+    slug: "shona",
+    coverImage: "/bgs/shona-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Introduce children to Shona with easy-to-follow vocabulary books.",
+  },
+  {
+    name: "Somali",
+    slug: "somali",
+    coverImage: "/bgs/somali-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Make learning Somali fun and easy for kids with beginner-friendly books.",
+  },
+  {
+    name: "Swahili",
+    slug: "swahili",
+    coverImage: "/bgs/swahili-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Help kids learn Swahili with simple and engaging learning books.",
+  },
+  {
+    name: "Twi",
+    slug: "twi",
+    coverImage: "/bgs/twi-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Introduce children to Twi with beginner-friendly vocabulary books.",
+  },
+  {
+    name: "Wolof",
+    slug: "wolof",
+    coverImage: "/bgs/wolof-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Teach kids Wolof with simple books focused on essential words.",
+  },
+  {
+    name: "Yoruba",
+    slug: "yoruba",
+    coverImage: "/bgs/yoruba-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Help children learn Yoruba with easy-to-understand word books.",
+  },
+  {
+    name: "Zulu",
+    slug: "zulu",
+    coverImage: "/bgs/zulu-bg.png",
+    amazonUrl: "#",
+    comingSoon: true,
+    discoverBooks:
+      "Make learning Zulu simple for kids with beginner vocabulary books.",
+  },
 ];
