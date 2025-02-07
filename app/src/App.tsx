a819a23f52d782543959c8a8e5e07a9f0ee7d95a
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-02-07 21:08:07 +0100

    Update default route path to "/lessons".

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route
-              path="/"
+              path="/lessons"
               element={
                 <PrivateRoute>
                   <Lessons />
