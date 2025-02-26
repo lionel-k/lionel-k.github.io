44d0c2dc05881c21f46597b70830cd591117a837
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-02-26 02:48:23 +0100

    Add blog link to footer component

--- www/components/footer.tsx
+++ www/components/footer.tsx
@@ -58,6 +58,14 @@ export function Footer() {
             <ul className="mt-4 space-y-2">
               <li>
                 <Link
+                  href="/blog"
+                  className="text-sm text-gray-300 hover:text-[#DAA520]"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
                   href="/about"
                   className="text-sm text-gray-300 hover:text-[#DAA520]"
                 >
