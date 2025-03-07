d88a1c71b5023f03d452dc9312ed566beff08410
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-03-07 21:28:45 +0100

    Add Bambara language to languagesConfig and related images

--- www/lib/languagesConfig.ts
+++ www/lib/languagesConfig.ts
@@ -1,7 +1,7 @@
 import { kirundiConfigs } from "./languageConfigs/kirundiConfigs";
 import { kinyarwandaConfigs } from "./languageConfigs/kinyarwandaConfigs";
 import { amharicConfigs } from "./languageConfigs/amharicConfigs";
-// import { bambaraConfigs } from "./languageConfigs/bambaraConfigs";
+import { bambaraConfigs } from "./languageConfigs/bambaraConfigs";
 
 export type LanguageConfig = {
   name: string;
@@ -23,8 +23,8 @@ export const languagesConfig: Record<string, LanguageConfig> = {
     ...amharicConfigs,
     description: `Learn Amharic with bilingual picture books. Experience Ethiopian culture through stories in Amharic and English/French. Made for families.`,
   },
-  // bambara: {
-  //   ...bambaraConfigs,
-  //   description: `Learn Bambara with bilingual picture books. Experience Malian culture through stories in Bambara and English/French. Made for families.`,
-  // },
+  bambara: {
+    ...bambaraConfigs,
+    description: `Learn Bambara with bilingual picture books. Experience Malian culture through stories in Bambara and English/French. Made for families.`,
+  },
 };
