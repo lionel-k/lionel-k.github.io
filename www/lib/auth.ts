27bbd6bd4da46ee5388a15999e4eebb17793da17
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-04-14 17:15:24 +0200

    Update SITE_URL to use environmental variable

--- www/lib/auth.ts
+++ www/lib/auth.ts
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase";
 
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export interface SignInWithOtpResponse {
   error: Error | null;
