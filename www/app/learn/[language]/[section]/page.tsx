cf400ef961287090b812bdf6b38bb033b1d19782
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-04-18 13:16:16 +0200

    Add metadata generation for section pages

--- www/app/learn/[language]/[section]/page.tsx
+++ www/app/learn/[language]/[section]/page.tsx
@@ -1,7 +1,9 @@
+import { Metadata } from "next";
 import { getFlashcardSet } from "@/lib/learn";
 import { LANGUAGES } from "@/lib/constants";
 import { sections } from "@/lib/learn/sections";
 import { notFound } from "next/navigation";
+import { SITE_URL } from "@/lib/constants";
 import SectionClient from "./SectionClient";
 
 type Props = {
@@ -10,6 +12,50 @@ type Props = {
     section: string;
   }>;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { language: langSlug, section: sectionId } = await params;
+  const language = LANGUAGES.find((l) => l.slug === langSlug);
+  const section = sections.find((s) => s.id === sectionId);
+
+  if (!language || !section) {
+    return {
+      title: "Section Not Found",
+    };
+  }
+
+  const pageTitle = `${section.title} in ${language.name} | Lingu.Africa`;
+  const description = `${section.description} Learn ${language.name} vocabulary with interactive flashcards and audio.`;
+  const languageImageUrl = `${SITE_URL}/images/${langSlug}/${langSlug}.png`;
+
+  return {
+    title: pageTitle,
+    description: description,
+    openGraph: {
+      title: pageTitle,
+      description: description,
+      url: `${SITE_URL}/learn/${langSlug}/${sectionId}`,
+      siteName: "Lingu.Africa",
+      images: [
+        {
+          url: languageImageUrl,
+          width: 1200,
+          height: 630,
+          alt: `${section.title} in ${language.name}`,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: pageTitle,
+      description: description,
+      images: [languageImageUrl],
+      creator: "@lionel.kubwimana",
+    },
+  };
+}
 
 export default async function SectionPage({ params }: Props) {
   const { language: langSlug, section: sectionId } = await params;
