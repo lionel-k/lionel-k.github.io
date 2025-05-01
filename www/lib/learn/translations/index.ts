580ea6382372e056cc73bc1221b5b42bc6039e96
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-05-01 21:33:20 +0200

    Remove unused kinyarwanda translation import

--- www/lib/learn/translations/index.ts
+++ www/lib/learn/translations/index.ts
@@ -1,8 +1,6 @@
 import { LanguageTranslations } from "../types";
 import { kirundi } from "./kirundi";
-import { kinyarwanda } from "./kinyarwanda";
 
 export const languageTranslations: Record<string, LanguageTranslations> = {
   kirundi,
-  kinyarwanda,
 };
